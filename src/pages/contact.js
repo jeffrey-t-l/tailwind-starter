4217680e900b87cf1e5e70e4f9024e6f73210e1d
Jeff Leibensperger <jeffrey.leibensperger@gmail.com>
2021-03-02 19:41:08 -0500

    adding new form to test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,6 +12,28 @@ function ContactPage() {
       />
       <section>
         <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </section>
+      <section>
+        <form name="contact" method="POST" data-netlify="true">
           <h2 className="mb-8 leading-loose">
             Here is an example of a form built using the official Tailwind CSS Forms plugin.{` `}
             <a
